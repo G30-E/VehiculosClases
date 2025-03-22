d98a764b99a36e560fe62ed59a987e6ffa734f58
G30-E <gfloriane@miumg.edu.gt>
2025-03-21 23:57:25 -0600

    proyecto y PDF subido

---
+++ Vehiculos.cs
@@ -0,0 +1,27 @@
+﻿using ProyectoVehiculos.Clases;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vehiculo miCarroElectrico = new CarroElectrico();
+        miCarroElectrico.Acelerar(50);
+        miCarroElectrico.Frenar(20);
+        ((CarroElectrico)miCarroElectrico).CargarBateria(30);
+
+        Vehiculo miAutoDeCombustion = new AutoDeCombustion();
+        miAutoDeCombustion.Acelerar(60);
+        miAutoDeCombustion.Frenar(30);
+        ((AutoDeCombustion)miAutoDeCombustion).CargarCombustible(20);
+
+        Vehiculo miMotocicleta = new Motocicleta();
+        miMotocicleta.Acelerar(80);
+        miMotocicleta.Frenar(40);
+        ((Motocicleta)miMotocicleta).UsarCasco();
+
+        Vehiculo miCamion = new Camion();
+        miCamion.Acelerar(70);
+        miCamion.Frenar(50);
+        ((Camion)miCamion).CargarMercancia(1000);
+    }
+}
